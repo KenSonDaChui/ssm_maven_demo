7150432df41d0beec2ca81ecd61279db4cc40169
wangdachui <wangdachui@hubei.com>
2018-10-16 19:36:40 +0800

    用户角色权限关联完成

--- ssm_web/src/main/java/com/itcast/ssm/controller/UserController.java
+++ ssm_web/src/main/java/com/itcast/ssm/controller/UserController.java
@@ -2,10 +2,10 @@ package com.itcast.ssm.controller;
 
 import com.itcast.ssm.domain.UserInfo;
 import com.itcast.ssm.service.IUserService;
-import com.itcast.ssm.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -21,7 +21,7 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
-    //查询用户的所有信息
+    //查询用户的所有信息(带有模糊查询)
     @RequestMapping("/findAll.do")
     public ModelAndView findAll() throws Exception {
         ModelAndView mv = new ModelAndView();
@@ -41,14 +41,38 @@ public class UserController {
     }
 
     //查找用户详情信息
+    @RequestMapping("/findUserByIdAndAllRole.do")
+    public ModelAndView findUserByIdAndAllRole(String id) throws Exception {
+        ModelAndView mv = new ModelAndView();
+       UserInfo us= userService.findUserAndOtherRole(id);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println(us);
+
+        mv.addObject("user",us);
+        mv.setViewName("user-role-add");
+        return mv;
+    }
+
+
+
     @RequestMapping("/findById.do")
     public ModelAndView findById(String id) throws Exception {
         ModelAndView mv = new ModelAndView();
         UserInfo userInfo =  userService.findById(id);
         System.out.println("===========================userInfo=========================");
         System.out.println(userInfo);
+
         mv.addObject("user",userInfo);
         mv.setViewName("user-show");
         return mv;
     }
+
+    @RequestMapping("/addRoleToUser.do")
+    public String addRoleToUser(@RequestParam(name = "userId",required = true) String id, @RequestParam(name = "ids",required = true) String[] roleIds) throws Exception {
+        userService.addRoleToUser(id,roleIds);
+
+
+        return "redirect:/user/findAll.do";
+    }
+
 }
